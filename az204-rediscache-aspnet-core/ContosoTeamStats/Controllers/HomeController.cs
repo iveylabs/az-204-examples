3eef2b6f73a0c9ab4784589f51fb0c16baab530d
Paul Ivey <paul@iveylabs.co.uk>
2022-09-20 17:14:16 +0100

    Added ASP.NET Redis example

---
+++ az204-rediscache-aspnet-core/ContosoTeamStats/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+using ContosoTeamStats.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ContosoTeamStats.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly Task<RedisConnection> _redisConnectionFactory;
+        private RedisConnection _redisConnection;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration, Task<RedisConnection> redisConnectionFactory)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _redisConnectionFactory = redisConnectionFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> RedisCache()
+        {
+            _redisConnection = await _redisConnectionFactory;
+            ViewBag.Message = "A simple example with Azure Cache for Redis on ASP.NET Core.";
+
+            // Perform cache operations using the cache object...
+
+            // Simple PING command
+            ViewBag.command1 = "PING";
+            ViewBag.command1Result = (await _redisConnection.BasicRetryAsync(async (db) => await db.ExecuteAsync(ViewBag.command1))).ToString();
+
+            // Simple get and put of integral data types into the cache
+            string key = "Message";
+            string value = "Hello, World!";
+
+            ViewBag.command2 = $"SET {key} \"{value}\"";
+            ViewBag.command2Result = (await _redisConnection.BasicRetryAsync(async (db) => await db.StringSetAsync(key, value))).ToString();
+
+            ViewBag.command3 = $"GET {key}";
+            ViewBag.command3Result = (await _redisConnection.BasicRetryAsync(async (db) => await db.StringGetAsync(key))).ToString();
+
+            key = "LastUpdateTime";
+            value = DateTime.UtcNow.ToString();
+
+            ViewBag.command4 = $"GET {key}";
+            ViewBag.command4Result = (await _redisConnection.BasicRetryAsync(async (db) => await db.StringGetAsync(key))).ToString();
+
+            ViewBag.command5 = $"SET {key}";
+            ViewBag.command5Result = (await _redisConnection.BasicRetryAsync(async (db) => await db.StringSetAsync(key, value))).ToString();
+
+            return View();
+        }
+    }
+}
