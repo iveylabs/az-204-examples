c9a6088d4254e6f516f63852804dc4f25af34be8
Paul Ivey <paul@iveylabs.co.uk>
2022-11-06 22:04:15 +0000

    Ready for day 1

---
+++ 01-App Services/03-Logging/LoggingDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoggingDemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("Custom log entry for the AZ-204 course");
+        }
+    }
+}
